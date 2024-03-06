1dcf87e4bb72891946491a00763d6f6ca6534e91
Mariusz Matusiewicz <mariuszmmm@op.pl>
2024-03-06 08:14:24 +0100

    test

--- App/routes/filesRoutes.js
+++ App/routes/filesRoutes.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 router.get('/files', (req, res) => {
   // const directoryPath = path.join(__dirname, 'public', 'Images');
-  const directoryPath = path.join(__dirname, '../public/Images');
+  const directoryPath = path.join(__dirname, 'public/Images');
 
 
   // Odczytaj zawartość folderu
