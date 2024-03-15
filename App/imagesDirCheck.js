66a40b0749a193083a6222fecad88880395d2df1
Mariusz Matusiewicz <mariuszmmm@op.pl>
2024-03-15 18:41:25 +0100

    test

--- App/imagesDirCheck.js
+++ App/imagesDirCheck.js
@@ -1,18 +1,12 @@
 const path = require("path");
 const fs = require('fs');
-require('dotenv').config();
-const DATABASENAME = process.env.DATABASENAME;
-
 
 function imagesDirCheck() {
-  const imagesDirectory = path.join(__dirname, `../${DATABASENAME}/public/Images`);
+  const imagesDirectory = path.join(__dirname, '../public/Images');
   if (!fs.existsSync(imagesDirectory)) {
     fs.mkdirSync(imagesDirectory, { recursive: true });
     console.log('Katalog "Images" został stworzony.');
   } else {
-
-    fs.mkdirSync(imagesDirectory, { recursive: true });
-
     console.log('Katalog "Images" już istnieje.');
   }
 };
