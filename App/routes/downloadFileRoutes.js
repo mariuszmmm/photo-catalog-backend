87ecd7d7d76664acb5e578b16ee9c26f51bcc24b
Mariusz Matusiewicz <mariuszmmm@op.pl>
2024-03-15 15:23:01 +0100

    change path to files

--- App/routes/downloadFileRoutes.js
+++ App/routes/downloadFileRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-router.get('/files/download/:nazwaPliku', (req, res) => {
+router.get('/files/:nazwaPliku', (req, res) => {
   const { filenName } = req.params;
   const directoryPath = `public/Images/${filenName}`;
   res.download(directoryPath);
 });
 
-module.exports = router;  
+module.exports = router;
