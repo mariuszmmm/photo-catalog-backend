ac238b5fcbbcf3b96067b50f89dffbf814012a35
Mariusz Matusiewicz <mariuszmmm@op.pl>
2024-10-29 06:04:59 +0100

    test

--- functions/api.js
+++ functions/api.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const app = express();
+
 const router = express.Router();
+
+// const mongoose = require('mongoose');
+// mongoose.connect(CONNECTION_STRING, {
+//   dbName: DATABASENAME
+// }).then(() => console.log('Połączono z bazą danych'))
+//   .catch((error) => console.error('Błąd podczas łączenia z bazą danych:', error));
+
 
 router.get("/", (req, res) => {
   res.send("Serwer działa!")
 });
 
-app.use('/api', router);
+router.get("/.netlify/functions/api", (req, res) => {
+  res.send("api działa!")
+});
+
+
+app.use('/', router);
 
 module.exports.handler = serverless(app);
